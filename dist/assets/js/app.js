ac5f3996ca6b9d615a92d6bb47cbe57245fefdb6
Daniel Stephany <danielstephany85@gmail.com>
2016-10-30 13:07:41 -0700

    finished fade animation timing

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -2388,11 +2388,23 @@ if (typeof jQuery === 'undefined') {
 slideSelectSS();
 
 function slideSelectSS(){
-	var slideSelector = $('.slideSelector');
+	var $slideSelector = $('.slide-selector');
 
 	$slideSelector.on('click', function(){
-		$this = $(this).index();	
-		console.log($this);
+		var $this = $(this);
+		if(!$this.hasClass('active')){
+			$this.siblings('.active').removeClass("active");
+			$this.addClass('active');
+			var $slideSelectorSS = $this.parent('.slide-select-selectors').parent('.slide-select-ss');
+			$slideSelectorSS.children('.slide-select-slides').children(".active").slideToggle(500,function(){
+				$(this).removeClass('active');
+			});
+			var i = $(this).index();	
+			$slideSelectorSS.children('.slide-select-slides').children("div").eq(i).slideToggle(500, function(){
+				$(this).addClass('active');
+			});
+		}
+		
 	});
-}
+}  
 //# sourceMappingURL=app.js.map
