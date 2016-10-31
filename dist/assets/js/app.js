65da46d366679258fe4d55805736433c9536cec5
Daniel Stephany <danielstephany85@gmail.com>
2016-10-30 20:28:48 -0700

    added the swipe controls to main.js

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -2388,23 +2388,45 @@ if (typeof jQuery === 'undefined') {
 slideSelectSS();
 
 function slideSelectSS(){
-	var $slideSelector = $('.slide-selector');
+	var fadetime = 400;
+	var slidetime = 400; 
 
-	$slideSelector.on('click', function(){
+	//sets opacity and display settings on slides.
+	$('.slide-select-slides div').css({"opacity": "0", "display": "none"});
+	$('.slide-select-slides .active').css({"opacity": "1", "display": "block"});
+
+	//set on click for the selector and use its index to target a the slide width the same index.
+	$('.slide-selector').on('click', function(){
 		var $this = $(this);
-		if(!$this.hasClass('active')){
+		var i = $this.index();
+		//check to see if this is the active slide toggle or if the slide is in the proccess of animating.
+		if(!$this.hasClass('active') && !$this.hasClass('animating')){
+			$this.siblings('.slide-selector').addClass('animating');
+			var $slideSelectorSS = $this.parent('.slide-select-selectors').parent('.slide-select-ss');
 			$this.siblings('.active').removeClass("active");
 			$this.addClass('active');
-			var $slideSelectorSS = $this.parent('.slide-select-selectors').parent('.slide-select-ss');
-			$slideSelectorSS.children('.slide-select-slides').children(".active").slideToggle(500,function(){
-				$(this).removeClass('active');
-			});
-			var i = $(this).index();	
-			$slideSelectorSS.children('.slide-select-slides').children("div").eq(i).slideToggle(500, function(){
-				$(this).addClass('active');
-			});
-		}
-		
-	});
+			//fade slide to opacity 0 then slide up(closed).
+			$slideSelectorSS.children('.slide-select-slides').children(".active").stop().animate({
+			    opacity: 0
+			  }, fadetime, function() {
+			    $(this).slideUp(slidetime,function(){
+							$(this).removeClass('active');
+						});
+			  });
+			//set setTimeout for duration of the opacity animation time, animate slide to opacity 1 then slide down(open).	
+			setTimeout(function(){
+				$slideSelectorSS.children('.slide-select-slides').children("div").eq(i).stop().slideDown(slidetime, function(){
+					$(this).animate({
+						opacity: 1
+					}, fadetime, function(){
+						$(this).addClass('active');
+						$this.siblings('.animating').removeClass('animating');
+					})
+				});
+			}, fadetime);//end setTimeout			
+		}//end if
+	});//end onclick
 }  
+
+//added slider like functionality for mobile friendly usage
 //# sourceMappingURL=app.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -24,7 +24,7 @@ function slideSelectSS(){
 			$slideSelectorSS.children('.slide-select-slides').children(".active").stop().animate({
 			    opacity: 0
 			  }, fadetime, function() {
-			    $(this).slideUp(slidetime,function(){
+			    $(this).slideUp(slidetime, function(){
 							$(this).removeClass('active');
 						});
 			  });
@@ -36,11 +36,72 @@ function slideSelectSS(){
 					}, fadetime, function(){
 						$(this).addClass('active');
 						$this.siblings('.animating').removeClass('animating');
-					})
+					});
 				});
 			}, fadetime);//end setTimeout			
 		}//end if
 	});//end onclick
+
+
+/* slide events **********************************************/
+
+	$('.slide-select-slides').children("div").on('swiperight', function(){
+		var $this = $(this);
+		var i = $this.index() - 1;
+		if (i === -1){
+			i = $this.parent('.slide-select-slides').children('div').length - 1;
+		}
+		slideControl($this, i);
+	});
+
+
+
+	$('.slide-select-slides').children("div").on('swipeleft', function(){
+		var $this = $(this);
+		var i = $this.index() + 1;
+		if (i === ($this.siblings('div').length + 1)){
+			i = 0;
+		}
+		slideControl($this, i);
+	});
+
+	//slide animation function to be used in the slide events
+	function slideControl($this, index){
+		if($this.hasClass('active') && !$this.hasClass('animating')){
+			$this.parent('.slide-select-slides').children('div').addClass('animating');
+			var $slideSelectorSS = $this.parent('.slide-select-slides').parent('.slide-select-ss');
+			$slideSelectorSS.children('.slide-select-selectors').children('.active').removeClass('active');
+			$slideSelectorSS.children('.slide-select-selectors').children('.slide-selector').eq(index).addClass('active');
+			$this.addClass('active');
+			//fade slide to opacity 0 then slide up(closed).
+			$this.stop().animate({
+			    opacity: 0
+			  }, fadetime, function() {
+			    $(this).slideUp(slidetime, function(){
+							$(this).removeClass('active');
+						});
+			  });
+			//set setTimeout for duration of the opacity animation time, animate slide to opacity 1 then slide down(open).	
+			setTimeout(function(){
+				$this.parent('.slide-select-slides').children('div').eq(index).stop().slideDown(slidetime, function(){
+					$(this).animate({
+						opacity: 1
+					}, fadetime, function(){
+						$(this).addClass('active');
+						$this.siblings('.animating').removeClass('animating');
+						$this.removeClass("active");
+					});
+				});
+			}, fadetime);//end setTimeout
+		}
+	}
 }  
 
-//added slider like functionality for mobile friendly usage
+
+
+
+
+
+
+
+
