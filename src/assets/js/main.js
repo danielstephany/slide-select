79bca5cec2db7661ec2f9b40351e91ea8f18799f
Daniel Stephany <Daniel@Daniels-MacBook-Pro-2.local>
2017-04-29 10:15:04 -0700

    changed the animation to be more efficiant

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,8 +3,7 @@
 slideSelectSS();
 
 function slideSelectSS(){
-	var fadetime = 400;
-	var slidetime = 400; 
+	var fadetime = 400; 
 
 	//sets opacity and display settings on slides.
 	$('.slide-select-slides div').css({"opacity": "0", "display": "none"});
@@ -13,42 +12,54 @@ function slideSelectSS(){
 	//set on click for the selector and use its index to target a the slide width the same index.
 	$('.slide-selector').on('click', function(){
 		var $this = $(this);
+		var $slideSelectorSS = $this.parent('.slide-select-selectors').parent('.slide-select-ss');
+		var $slideselectslides = $slideSelectorSS.children('.slide-select-slides');
 		var i = $this.index();
 		//check to see if this is the active slide toggle or if the slide is in the proccess of animating.
 		if(!$this.hasClass('active') && !$this.hasClass('animating')){
 			$this.siblings('.slide-selector').addClass('animating');
-			var $slideSelectorSS = $this.parent('.slide-select-selectors').parent('.slide-select-ss');
 			$this.siblings('.active').removeClass("active");
 			$this.addClass('active');
-			//fade slide to opacity 0 then slide up(closed).
-			/**
-			 * animation sequence
-			 * get the current hieght of slide-select-slides and set the height the current height and overflow hidden
-			 * fade out the current active div and set to display none
-			 * then display the new div but keep it opacity 0 
-			 * then get the height of the new active div and animate slide-select-slides to the new height and remove overflow hidden
-			 * finaly set the opacity of the active div to 1
-			 */
-			$slideSelectorSS.children('.slide-select-slides').children(".active").stop().animate({
+	
+			//get the current hieght of slide-select-slides and set the height to the current height and overflow hidden
+			var slideHeight = $slideselectslides.outerHeight(true);
+			$slideselectslides.css({"height": slideHeight, "overflow": "hidden" });
+				//fade out the current active div and set to display none
+				$slideselectslides.children(".active").stop().animate({
 			    opacity: 0
 			  }, fadetime, function() {
-			    $(this).slideUp(slidetime, function(){
-							$(this).removeClass('active');
-						});
+			    $(this).css({"display": "none"}).removeClass('active');
 			  });
-			//set setTimeout for duration of the opacity animation time, animate slide to opacity 1 then slide down(open).	
+			
 			setTimeout(function(){
-				$slideSelectorSS.children('.slide-select-slides').children("div").eq(i).stop().slideDown(slidetime, function(){
-					$(this).animate({
-						opacity: 1
-					}, fadetime, function(){
-						$(this).addClass('active');
-						$this.siblings('.animating').removeClass('animating');
-					});
+				//then display the new div but keep its opacity 0
+				var $currentContent = $slideselectslides.children("div").eq(i);
+				$currentContent.css({"display": "block"}).addClass('active');
+				//then get the height of the new active div and animate slide-select-slides to the new height and remove overflow hidden
+				var contentHeight = $currentContent.outerHeight(true);	
+				$slideselectslides.stop().animate({
+			    height: contentHeight
+			  }, fadetime);
+			  setTimeout(function(){
+  				$currentContent.animate({
+					opacity: 1 //finaly set the opacity of the active div to 1
+				},fadetime, function() {
+					$(this).addClass('active');
+					$this.siblings('.animating').removeClass('animating');
 				});
-			}, fadetime);//end setTimeout			
+			  },fadetime);
+			}, fadetime);
+			$slideselectslides.css({"overflow": "visible" });		
 		}//end if
 	});//end onclick
+
+	$(window).on('resize', function(){
+		$('.slide-select-slides .active').each(function(i){
+			var $this = $(this);
+			var resetHeight = $this.outerHeight(true);
+			$this.parent("div").css({"height": resetHeight});
+		});
+	});
 
 
 /* slide events **********************************************/
@@ -78,29 +89,40 @@ function slideSelectSS(){
 		if($this.hasClass('active') && !$this.hasClass('animating')){
 			$this.parent('.slide-select-slides').children('div').addClass('animating');
 			var $slideSelectorSS = $this.parent('.slide-select-slides').parent('.slide-select-ss');
-			$slideSelectorSS.children('.slide-select-selectors').children('.active').removeClass('active');
-			$slideSelectorSS.children('.slide-select-selectors').children('.slide-selector').eq(index).addClass('active');
+			var $slideselectslides = $slideSelectorSS.children('.slide-select-slides');
+			var $slideselectors = $slideSelectorSS.children('.slide-select-selectors');
+			$slideselectors.children('.active').removeClass('active');
+			$slideselectors.children('.slide-selector').eq(index).addClass('active');
 			$this.addClass('active');
-			//fade slide to opacity 0 then slide up(closed).
-			$this.stop().animate({
+	
+			var slideHeight = $slideselectslides.outerHeight(true);
+			$slideselectslides.css({"height": slideHeight, "overflow": "hidden" });
+				//fade out the current active div and set to display none
+				$slideselectslides.children(".active").stop().animate({
 			    opacity: 0
 			  }, fadetime, function() {
-			    $(this).slideUp(slidetime, function(){
-							$(this).removeClass('active');
-						});
+			    $(this).css({"display": "none"}).removeClass('active');
 			  });
-			//set setTimeout for duration of the opacity animation time, animate slide to opacity 1 then slide down(open).	
+			
 			setTimeout(function(){
-				$this.parent('.slide-select-slides').children('div').eq(index).stop().slideDown(slidetime, function(){
-					$(this).animate({
-						opacity: 1
-					}, fadetime, function(){
-						$(this).addClass('active');
-						$this.siblings('.animating').removeClass('animating');
-						$this.removeClass("active");
-					});
+				//then display the new div but keep its opacity 0
+				var $currentContent = $slideselectslides.children("div").eq(index);
+				$currentContent.css({"display": "block"}).addClass('active');
+				//then get the height of the new active div and animate slide-select-slides to the new height and remove overflow hidden
+				var contentHeight = $currentContent.outerHeight(true);	
+				$slideselectslides.stop().animate({
+			    height: contentHeight
+			  }, fadetime);
+			  setTimeout(function(){
+  				$currentContent.animate({
+					opacity: 1 //finaly set the opacity of the active div to 1
+				},fadetime, function() {
+					$(this).addClass('active');
+					$this.siblings('.animating').removeClass('animating');
 				});
-			}, fadetime);//end setTimeout
+			  },fadetime);
+			}, fadetime);
+			$slideselectslides.css({"overflow": "visible" });
 		}
 	}
 }  
